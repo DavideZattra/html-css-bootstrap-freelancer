3cab87f8cdd1a766569d8a85f8a12c771842593d
DavideZattra <d.zattra@gmail.com>
2021-08-03 16:03:30 +0200

    added javascript file and changes to titles

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+document.getElementById("ms_navbar-written-logo").innerHTML = "Start bootstrap & js";
+
+document.getElementById("ms_under-avatar-title").innerHTML = "start bootstrap and js";
+
+document.getElementById("ms_about-title").innerHTML = "about bootstrap and js";
